040cc0eb3b74578eb2866602d603c05135f9f8e9
Andrei <andrej.daxnov@bk.ru>
2025-11-26 17:29:50 +0300

    Trained yolo, create alembic migrations

--- ai_model/train.py
+++ ai_model/train.py
@@ -5,19 +5,19 @@ import multiprocessing as mp
 
 def main():
     dataset = "C:\\Users\\User\\Desktop\\proj_1\\ai_model\\plant-disease-1"
-    model = YOLO('yolo11s.pt')
+    model = YOLO('C:\\Users\\User\\Desktop\\proj_1\\runs\\detect\\plant_disease_exp3\\weights\\best.pt')
 
     data_yaml = os.path.join(dataset, 'data.yaml')
 
     model.train(
         data=data_yaml,
-        epochs=100,
+        epochs=150,
         imgsz=640,
         batch=16,
-        name='plant_disease_exp2',
+        name='plant_disease_exp4',
         device=0,
         workers=4,
-        pretrained=True
+        pretrained=False
     )
 
 
